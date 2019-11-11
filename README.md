61fefee20efbaef9c9b3b794ad82b4d9e4a48af4
Ayman <aymankhawaldeh8@gmail.com>
2019-11-11 16:02:11 +0200

    NEW WORK

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # About-me
+
+*i made a site that contain informations about me and before the site open i added a yes/no questions with javaScript and after that the site will open and there is informations about me ,and about the steps i made also files like .gitignore and .eslintrc.json and also made a reset file , and i must say that thanks for samer and bashar and sheab cause they helped me*
+
+## Commands 
+1. // comment
+2. alert
+3. switch
+4. var
+5. console.log
+
+## Built with :
+*VSS code*
+
+## LICENSe :
+
+*and i used LICENSE  to ensure that anyone who uses your code knows the rights and limitations of that use. For general use, the broad open-source MIT license works well.*
+
+
+## Acknowledgements :
+from here
+
+[.eslintrc.json](https://github.com/LTUC/amman-201d2/blob/master/configs/.eslintrc.json)
+[reset.css](https://meyerweb.com/eric/tools/css/reset/)
+
+
+**thanks for reading**
+
---
+++ js/app.js
@@ -0,0 +1,162 @@
+'use strict'
+
+var name = prompt(' what is your name? ');
+alert(' welocme ' + name + ' i wanna ask u 5 questions and u must answer with yes/no also y/n ');
+
+var answer = prompt(' We have 7 skyes ? ')
+var answer = answer.toUpperCase();
+switch (answer) {
+    case 'YES':
+        alert(' correct');
+        //console.log( ' wow thats true ')
+
+        break;
+    case 'Y':
+        alert(' correct');
+        //console.log( ' wow thats true ')
+
+        break;
+    case 'NO':
+        alert(' incorrect');
+        //console.log( ' incorrect  ')
+
+        break;
+    case 'N':
+        alert(' incorrect');
+        //console.log( ' incorrect  ')
+
+        break;
+
+    default:
+        alert(' wrong answer ');
+        //console.log( ' wrong  ')
+
+        break;
+}
+
+var answer2 = prompt(' misse is  bitter than ronaldo ? ')
+var answer2 = answer2.toUpperCase();
+switch (answer2) {
+    case 'YES':
+        alert(' correct');
+        //console.log( ' wow thats true ')
+
+        break;
+    case 'Y':
+        alert(' correct');
+        //console.log( ' wow thats true ')
+
+        break;
+    case 'NO':
+        alert(' incorrect');
+        break;
+    case 'N':
+        alert(' incorrect');
+        break;
+
+    default:
+        alert(' wrong answer ');
+        //console.log( ' wrong  ')
+
+        break;
+}
+//console.log( ' wow thats true ')
+
+
+var answer3 = prompt(' Allah is the only god ? ')
+var answer3 = answer3.toUpperCase();
+switch (answer2) {
+    case 'YES':
+        alert(' correct');
+        //console.log( ' wow thats true ')
+
+        break;
+    case 'Y':
+        alert(' correct');
+        //console.log( ' wow thats true ')
+
+        break;
+    case 'NO':
+        alert(' incorrect answer');
+        //console.log( ' incorrect  ')
+
+        break;
+    case 'N':
+        alert(' incorrect answer');
+        //console.log( ' incorrect  ')
+
+        break;
+
+    default:
+        alert(' wrong answer ');
+        //console.log( ' wrong  ')
+
+        break;
+}
+
+
+var answer4 = prompt(' are you beautiful ? ')
+var answer4 = answer4.toUpperCase();
+switch (answer2) {
+    case 'YES':
+        alert(' correct');
+        //console.log( ' wow thats true ')
+
+        break;
+    case 'Y':
+        alert(' correct');
+        //console.log( ' wow thats true ')
+
+        break;
+    case 'NO':
+        alert(' incorrect answer');
+        //console.log( ' incorrect  ')
+
+        break;
+    case 'N':
+        alert(' incorrect answer');
+        //console.log( ' incorrect  ')
+
+        break;
+
+    default:
+        alert(' wrong answer ');
+        //console.log( ' wrong  ')
+
+        break;
+}
+
+var answer5 = prompt(' are you beautiful ? ')
+var answer5 = answer5.toUpperCase();
+switch (answer5) {
+    case 'YES':
+        alert(' correct');
+        //console.log( ' wow thats true ')
+
+        break;
+    case 'Y':
+        alert(' correct');
+        //console.log( ' wow thats true ')
+
+        break;
+    case 'NO':
+        alert(' incorrect answer');
+        //console.log( ' incorrect  ')
+
+        break;
+    case 'N':
+        alert(' incorrect answer');
+        //console.log( ' incorrect  ')
+
+        break;
+
+    default:
+        alert(' wrong answer ');
+        //console.log( ' wrong  ')
+
+
+        break;
+}
+
+
+alert(' Welcome again ' + name);
